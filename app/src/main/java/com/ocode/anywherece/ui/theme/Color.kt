16dbc7b5cef0e45c72652eb8bfafed900ef3458c
Osayuki's Work <oethe68@gmail.com>
2022-07-26 17:55:48 -0400

    Initial commit

---
+++ app/src/main/java/com/ocode/anywherece/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.ocode.anywherece.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Navy200 = Color(0xFF444886)
+val Navy500 = Color(0xFF122159)
+val Navy700 = Color(0xFF000030)
+val Peach200 = Color(0xFFFFC686)
+
+
+// <color name="primaryColor">#122159</color>
+//  <color name="primaryLightColor">#444886</color>
+//  <color name="primaryDarkColor">#000030</color>
+//  <color name="secondaryColor">#ff9558</color>
+//  <color name="secondaryLightColor">#ffc686</color>
+//  <color name="secondaryDarkColor">#c7662b</color>
+//  <color name="primaryTextColor">#ffffff</color>
+//  <color name="secondaryTextColor">#000000</color>
